77b34984a70dfb1eac79adc457429c0217427b5b
San2Agar <sanagarwal2048@gmail.com>
2026-05-31 21:00:01 +0530

    train function with batch size

--- micrograd/nn.py
+++ micrograd/nn.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 from engine import Value
 
 class Module:   # to replicate pytorch structure
@@ -60,25 +61,53 @@ class MLP(Module):
     def __repr__(self):
         return f"MLP of [{', '.join(str(layer) for layer in self.layers)}]"
     
-    def train(self, train_x, train_y, lr = 0.05, niters = 20):
+    def train(self, train_x, train_y, lr = 0.05, niters = 20, batch_size = None):
 
         if(train_x is None or train_y is None): raise ValueError("dataset values missing")
         
-        print("SNo \t Prediction Loss")
+        # loss function
+        def loss():
+            if batch_size is None:
+                Xb, yb = train_x, train_y
+            else:
+                ri = np.random.permutation(train_x.shape[0])[:batch_size]
+                Xb, yb = train_x[ri], train_y[ri]
+            inputs = [list(map(Value, xrow)) for xrow in Xb]
+            
+            # forward the model to get scores
+            scores = list(map(self, inputs))
+            
+            # svm "max-margin" loss
+            losses = [(1 + -yi*scorei).tanh() for yi, scorei in zip(yb, scores)]
+            data_loss = sum(losses) * (1.0 / len(losses))
+            # L2 regularization
+            alpha = 1e-4
+            reg_loss = alpha * sum((p*p for p in self.parameters()))
+            total_loss = data_loss + reg_loss
+            
+            # also get accuracy
+            accuracy = [(yi > 0) == (scorei.data > 0) for yi, scorei in zip(yb, scores)]
+            return total_loss, sum(accuracy) / len(accuracy)
+
         for i in range(niters):
             #forward pass
-            ypred = [self(x) for x in train_x]
-            loss = sum((ygt - yout)**2 for ygt, yout in zip(train_y, ypred))
+            total_loss, acc = loss()
+            """
+                ypred = [self(x) for x in train_x]
+                loss = sum((ygt - yout)**2 for ygt, yout in zip(train_y, ypred))
+            """
 
             #backward pass
             for p in self.parameters():    # if not used this would accumulate the grdaients from previous step
                 p.grad = 0.0            # and we will have a gradient descent step size that will explode with each trainign step
-            loss.backward()
+            # or model.zero_grad()
+            total_loss.backward()
 
             #update
+            lr = lr * 0.95 if (i%10 == 0) else lr  #reduce lr as we move towards center of minima
             for p in self.parameters():
-                p.data += -lr * p.grad
+                p.data -= lr * p.grad
 
-            print(i, '\t', loss.data)
+            print(f"step {i} loss {total_loss.data}, accuracy {acc*100}%")
 
-        return ypred
+        return [self(x) for x in train_x]
